b871aeca1ce532377c626b60fc2f6757e946f74b
AleksK <a.karagodin.001@gmail.com>
2026-04-22 18:19:32 +0300

    Add file1

---
+++ python_trening/task_8_if.py
@@ -0,0 +1,63 @@
+#Программа проверяет является ли число положительным
+#или отрицательным и выводит соответствующее собщение
+#
+# num = 3
+#
+#
+#  # так же попробуйте следующие варианты значения num.
+# # num = -5
+# # num = 0
+#
+# if num >=  0:
+#     print('Число больше  либо равно 0')
+# else:
+#      print('Число отрицательное')
+
+
+# def task_yes_no(str_1, str_2):
+#     if str_1 in str_2:
+#         print('ДА')
+#     else:
+#         print('НЕТ')
+# task_yes_no(str_1='test', str_2='test1')
+
+# num_float=3.4
+# так же попробовать варианты
+# num_float=0
+# num_float=-4.5
+#
+# if num_float >  0:
+#     print('Положительное число')
+# elif num_float == 0:
+#     print('Ноль')
+# else:
+#      print('Число отрицательное')
+
+# num = 3
+# permit_print = True
+# if num > 0 and permit_print:
+#     print("num - положительное число")
+# elif not permit_print:
+#     print('Печать завершена')
+
+
+# def student_rank(year_of_study):
+#     if year_of_study == 1 or year_of_study == 2 or year_of_study == 3 or year_of_study ==4:
+#         print('Вы - бакалавр')
+#     elif year_of_study in range(5,7):
+#         print('Вы -магистр')
+#     elif 7 <= year_of_study <= 9:
+#         print('Вы - аспирант')
+#     else:
+#         print('ВВедите корректный год обучения')
+#
+# student_rank(3)
+
+
+a = 5
+if a>100 or a<-100:
+    print('-')
+else:
+    print('+')
+
+
